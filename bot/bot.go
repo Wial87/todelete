3a0796e30e97b1e26f412db8469a9517983433b5
Wial87 <63091447+Wial87@users.noreply.github.com>
2020-04-15 14:42:30 +0300

    Update bot.go

--- bot/bot.go
+++ bot/bot.go
@@ -80,7 +80,7 @@ func start(token, triggerWord string) {
 
 			for _, v := range messages {
 				if v != message.MessageID {
-					_, err := vk.MessagesEdit(api.Params{"peer_id": message.PeerID, "message_id": v, "message": "ᅠ"})
+					_, err := vk.MessagesEdit(api.Params{"peer_id": message.PeerID, "message_id": v, "message": "[deleted]"})
 					if err == nil {
 						count++
 						log.Printf("Edited %v message\n", count)
