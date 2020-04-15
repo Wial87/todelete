00711427f3bafdceb38e56a30801c59f2d298937
Wial87 <63091447+Wial87@users.noreply.github.com>
2020-04-15 14:40:19 +0300

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	accounts := map[string]string{
 		// "token": "keyWord",
-		"045767c47a6a70dad8ebce219b1a1b468505d31255ca150a24069fa07145d66a1ed9b8a2646ad7042e56e": "дд", // Это пример, поля не должны быть пустыми!!!
+		"addbff29de1c7adb5b9dbc20cbb95a9cd22e4da38235220af230e0b6f46f872fb6a65afe2650abbfa9cab": "хуй", // Это пример, поля не должны быть пустыми!!!
 	}
 
 	// Функция запуска аккаунтов
